807f1c2dc0f5068ce233f2de428aacc71f36b199
Mahdi Jafarkhani <Mahdi.Jafarkhani@Gmail.com>
2023-11-17 19:28:35 +0100

    Add trace_writes

--- src/dynamicslicing/trace_writes.py
+++ src/dynamicslicing/trace_writes.py
@@ -1,1 +1,11 @@
-# Implement a DynaPyt analysis to trace all writes by printing them to stdout.
+from typing import Callable, List, Any
+from dynapyt.analyses.BaseAnalysis import BaseAnalysis
+
+class TraceWritesAnalysis(BaseAnalysis):
+    def __init__(self) -> None:
+        super(TraceWritesAnalysis, self).__init__()
+
+    def write(
+        self, dyn_ast: str, iid: int, old_vals: List[Callable], new_val: Any
+    ) -> Any:
+        print(f'{new_val}')
