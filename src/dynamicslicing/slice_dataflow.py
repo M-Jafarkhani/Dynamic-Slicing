0d4d7c707e7d1bddb03efd282b146d7307ac69be
Mahdi Jafarkhani <Mahdi.Jafarkhani@Gmail.com>
2023-11-29 19:13:03 +0100

    Init slice_dataflow

--- src/dynamicslicing/slice_dataflow.py
+++ src/dynamicslicing/slice_dataflow.py
@@ -1,9 +1,39 @@
 import libcst as cst
+from libcst._nodes.module import Module
 from dynapyt.analyses.BaseAnalysis import BaseAnalysis
 from dynapyt.instrument.IIDs import IIDs
+from libcst.metadata import (
+    ParentNodeProvider,
+    PositionProvider,
+)
+class CodeAnalyzerWrapper(cst.CSTTransformer):
+    METADATA_DEPENDENCIES = (
+        ParentNodeProvider,
+        PositionProvider,
+    )
 
 class SliceDataflow(BaseAnalysis):
     def __init__(self, source_path):
-        with open(source_path, "r") as file:
-            source = file.read()
-        iid_object = IIDs(source_path)
+        super(SliceDataflow, self).__init__()
+        if (source_path.endswith("program.py")) :
+            self.iid_object = IIDs(source_path)
+            self.source_path = source_path
+            with open(source_path, "r") as file:
+                self.source = file.read()
+            syntax_tree = self.create_syntax_tree()
+            new_code = self.slice_code(syntax_tree)
+            self.create_sliced_file(new_code)
+
+    def create_syntax_tree(self) -> Module:
+        return cst.parse_module(self.source)
+    
+    def slice_code(self, syntax_tree: Module) -> str:
+        wrapper = cst.metadata.MetadataWrapper(syntax_tree)
+        code_modifier = CodeAnalyzerWrapper()
+        new_syntax_tree = wrapper.visit(code_modifier)
+        return new_syntax_tree.code
+
+    def create_sliced_file(self, sliced_code: str) -> None:
+        file_path = self.source_path.replace("/program.py", "/sliced.py")
+        with open(file_path, 'w') as file:
+            file.write(sliced_code)
