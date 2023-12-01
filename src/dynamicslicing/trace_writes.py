31a8d62a178466aa6a0d35299924e3edd58ffa4e
Mahdi Jafarkhani <Mahdi.Jafarkhani@Gmail.com>
2023-12-01 21:23:13 +0100

    Refactor slicing

--- src/dynamicslicing/trace_writes.py
+++ src/dynamicslicing/trace_writes.py
@@ -1,84 +1,106 @@
 import libcst as cst
-from typing import Callable, List, Any, Union, Tuple
+from typing import Callable, Dict, List, Any, Union, Tuple
 from dynapyt.analyses.BaseAnalysis import BaseAnalysis
 from dynapyt.utils.nodeLocator import get_node_by_location
 from utils import get_slicing_criterion_line
+
+
 class VariableMetaData():
-    name: str
-    firstDefinition: int
+    active_definition: int
+    previous_definition: int
 
-    def __init__(self, name: str, firstDefinition: int) -> None:
-        self.name = name
-        self.firstDefinition = firstDefinition
+    def __init__(self, active_definition: int) -> None:
+        self.active_definition = active_definition
+        self.previous_definition = -1
+
+
 class LineMetaData():
-    number: int
     dependencies: List[int] = []
+    slice_computed: bool
 
-    def __init__(self, number: int, dependencies: List[int]) -> None:
-        self.number = number
+    def __init__(self, dependencies: List[int]) -> None:
         self.dependencies = dependencies
+        self.slice_computed = False
+
 
 class TraceWritesAnalysis(BaseAnalysis):
+    lines_info: Dict[int, LineMetaData] = dict()
+    variables_info: Dict[str, VariableMetaData] = dict()
+
     def __init__(self) -> None:
         super(TraceWritesAnalysis, self).__init__()
-        self.linesInfo : List[LineMetaData] = list()
-        self.variablesInfo : List[VariableMetaData] = list()
 
     def read(self, dyn_ast: str, iid: int, val: Any) -> Any:
-        location = self.iid_to_location(dyn_ast,iid)
+        location = self.iid_to_location(dyn_ast, iid)
         if (location.start_line != location.end_line):
             return
-        readVariables = self.extract_variables(dyn_ast,iid)
-        
-        if (readVariables is not None):
+        read_variables = self.extract_variables(dyn_ast, iid)
+        if (read_variables is not None):
             dependencies: List[int] = []
-            for variable in readVariables:
-               
-                for variable_metadata in self.variablesInfo:
-                    if variable_metadata.name == variable:
-                        dependencies.append(variable_metadata.firstDefinition)
-            found = False
-            for lineInfo in self.linesInfo:
-                if lineInfo.number == location.start_line:
-                    lineInfo.dependencies += dependencies
-                    found = True
-            if not found:
-                self.linesInfo.append(LineMetaData(location.start_line, dependencies))
+            for variable in read_variables:
+                for key, value in self.variables_info.items():
+                    if key == variable:
+                        dependencies.append(value.active_definition)
+
+            if location.start_line in self.lines_info:
+                self.lines_info.get(
+                    location.start_line).dependencies += dependencies
+            else:
+                self.lines_info[location.start_line] = LineMetaData(
+                    dependencies)
 
     def write(self, dyn_ast: str, iid: int, old_vals: List[Callable], new_val: Any) -> Any:
-        location = self.iid_to_location(dyn_ast,iid)
+        location = self.iid_to_location(dyn_ast, iid)
         if (location.start_line != location.end_line):
             return
-        writeVariable = self.extract_variables(dyn_ast,iid)
-        if ((writeVariable is not None) and len(writeVariable) == 1):
-            found = False
-            for variable_metadata in self.variablesInfo:
-                if variable_metadata.name == writeVariable[0]:
-                    variable_metadata.firstDefinition = location.start_line
-                    found = True
-            if (not found):
-                self.variablesInfo.append(VariableMetaData(writeVariable[0],location.start_line))
+        write_variable = self.extract_variables(dyn_ast, iid)
+        if ((write_variable is not None) and len(write_variable) == 1):
+            if (write_variable[0] in self.variables_info):
+                self.variables_info[write_variable[0]
+                                    ].previous_definition = self.variables_info[write_variable[0]
+                                                                                ].active_definition
+                self.variables_info[write_variable[0]
+                                    ].active_definition = location.start_line
+            else:
+                self.variables_info[write_variable[0]
+                                    ] = VariableMetaData(location.start_line)
+
+    def augmented_assignment(self, dyn_ast: str, iid: int, left: Any, op: str, right: Any) -> Any:
+        left_variable = self.extract_variables(dyn_ast, iid)
+        location = self.iid_to_location(dyn_ast, iid)
+        if (left_variable is not None):
+            dependencies: List[int] = []
+            for key, value in self.variables_info.items():
+                if key == left_variable[0]:
+                    dependencies.append(value.previous_definition)
+
+            if location.start_line in self.lines_info:
+                self.lines_info.get(
+                    location.start_line).dependencies += dependencies
+            else:
+                self.lines_info[location.start_line] = LineMetaData(
+                    dependencies)
 
     def read_attribute(self, dyn_ast: str, iid: int, base: Any, name: str, val: Any) -> Any:
-        location = self.iid_to_location(dyn_ast,iid)
+        pass
 
     def read_subscript(self, dyn_ast: str, iid: int, base: Any, sl: List[Union[int, Tuple]], val: Any) -> Any:
-        location = self.iid_to_location(dyn_ast,iid)
+        pass
 
     def end_execution(self) -> None:
-        # for v in self.variablesInfo:
-        #     print(f"{v.name} -- {v.firstDefinition}")
-        # for i in self.linesInfo:
-        #     print(f"{i.number} -- {i.dependencies}")
-         
+        for key, value in self.variables_info.items():
+            print(f"{key} -- {value.active_definition}")
+        for key, value in self.lines_info.items():
+            print(f"{key} -- {value.dependencies}")
         filePath = next(iter(self.asts))
-        print(get_slicing_criterion_line(filePath)) 
+        slice_line_number = get_slicing_criterion_line(filePath)
+        numbers_to_keep = self.compute_slice(slice_line_number)
+        print(f"Number To Keep = {numbers_to_keep}")
 
     def extract_variables(self, dyn_ast: str, iid: int) -> List[str]:
-        location = self.iid_to_location(dyn_ast,iid)
+        location = self.iid_to_location(dyn_ast, iid)
         node = get_node_by_location(self._get_ast(dyn_ast)[0], location)
-        variables : List[str] = []
-
+        variables: List[str] = []
         if isinstance(node, cst.Name):
             variables.append(node.value)
         elif isinstance(node, cst.Assign):
@@ -87,8 +109,17 @@ class TraceWritesAnalysis(BaseAnalysis):
                     variables.append(target.target.value)
         elif isinstance(node, cst.AugAssign):
             if isinstance(node.target, cst.Name):
-                variables.append(node.target.value)         
+                variables.append(node.target.value)
+        return variables
 
-        return variables   
-          
-    
+    def compute_slice(self, slice_line_number: int) -> List[int]:
+        result: List[int] = list()
+        result.append(slice_line_number)
+        if (slice_line_number in self.lines_info):
+            if (self.lines_info[slice_line_number].slice_computed == False):
+                self.lines_info[slice_line_number].slice_computed = True
+                result = result + \
+                    self.lines_info[slice_line_number].dependencies
+                for item in self.lines_info[slice_line_number].dependencies:
+                    result = result + self.compute_slice(item)
+        return list(set(result))
--- tests/milestone1/task_1/main.py
+++ tests/milestone1/task_1/main.py
@@ -1,10 +1,23 @@
-def slice_me(): 
-    x=5
-    print("Hello World")
-    if x < 10:
-        x += 5 
-    y=0
-    return y
+# DYNAPYT: DO NOT INSTRUMENT
 
-slice_me()
 
+import dynapyt.runtime as _rt
+
+_dynapyt_ast_ = "/Users/mahdijafarkhani/Documents/Master's/Semester 02/PA/Dynamic-Slicing/tests/milestone1/task_1/main.py" + ".orig"
+try:
+    def slice_me():
+        x = _rt._write_(_dynapyt_ast_, 1, 1, [lambda: x])
+        y = _rt._write_(_dynapyt_ast_, 2, 2, [lambda: y])
+        x = _rt._write_(_dynapyt_ast_, 5, _rt._read_(_dynapyt_ast_, 3, lambda: x) + _rt._read_(_dynapyt_ast_, 4, lambda: y), [lambda: x]) 
+        z = _rt._write_(_dynapyt_ast_, 8, _rt._read_(_dynapyt_ast_, 6, lambda: x) - _rt._read_(_dynapyt_ast_, 7, lambda: y), [lambda: z])
+        z = _rt._write_(_dynapyt_ast_, 9, 1, [lambda: z])
+        x *= _rt._aug_assign_(_dynapyt_ast_, 10, lambda: x, 9, 4)
+        y += _rt._aug_assign_(_dynapyt_ast_, 31, lambda: y, 0, 2 + _rt._read_(_dynapyt_ast_, 30, lambda: z)) # slicing criterion
+        x = _rt._write_(_dynapyt_ast_, 12, 0, [lambda: x])    
+        z = _rt._write_(_dynapyt_ast_, 14, _rt._read_(_dynapyt_ast_, 13, lambda: y), [lambda: z])
+        y = _rt._write_(_dynapyt_ast_, 16, _rt._read_(_dynapyt_ast_, 15, lambda: x), [lambda: y])
+        return _rt._read_(_dynapyt_ast_, 17, lambda: y) 
+    
+    _rt._read_(_dynapyt_ast_, 18, lambda: slice_me)()
+except Exception as _dynapyt_exception_:
+    _rt._catch_(_dynapyt_exception_)
