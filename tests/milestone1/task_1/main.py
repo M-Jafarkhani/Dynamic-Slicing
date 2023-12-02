fbb6bee308272cb33451982d19fee581a07ce4a6
Mahdi Jafarkhani <Mahdi.Jafarkhani@Gmail.com>
2023-12-02 11:45:07 +0100

    Remove analysis files

--- tests/milestone1/task_1/main.py
+++ tests/milestone1/task_1/main.py
@@ -1,25 +1,14 @@
-# DYNAPYT: DO NOT INSTRUMENT
+def slice_me():
+    x = 1
+    y = 2
+    x = x + y 
+    z = x - y
+    z = 1
+    x *= 4
+    y += 2 + z # slicing criterion
+    x = 0    
+    z = y
+    y = x
+    return y 
 
-
-import dynapyt.runtime as _rt
-
-_dynapyt_ast_ = "/Users/mahdijafarkhani/Documents/Master's/Semester 02/PA/Dynamic-Slicing/tests/milestone1/task_1/main.py" + ".orig"
-try:
-    def slice_me():
-        _rt._func_entry_(_dynapyt_ast_, 0, [], "slice_me")
-        x = _rt._write_(_dynapyt_ast_, 1, 1, [lambda: x])
-        y = _rt._write_(_dynapyt_ast_, 2, 2, [lambda: y])
-        x = _rt._write_(_dynapyt_ast_, 5, _rt._read_(_dynapyt_ast_, 3, lambda: x) + _rt._read_(_dynapyt_ast_, 4, lambda: y), [lambda: x]) 
-        z = _rt._write_(_dynapyt_ast_, 8, _rt._read_(_dynapyt_ast_, 6, lambda: x) - _rt._read_(_dynapyt_ast_, 7, lambda: y), [lambda: z])
-        z = _rt._write_(_dynapyt_ast_, 9, 1, [lambda: z])
-        x *= _rt._aug_assign_(_dynapyt_ast_, 10, lambda: x, 9, 4)
-        y += _rt._aug_assign_(_dynapyt_ast_, 31, lambda: y, 0, 2 + _rt._read_(_dynapyt_ast_, 30, lambda: z)) # slicing criterion
-        x = _rt._write_(_dynapyt_ast_, 12, 0, [lambda: x])    
-        z = _rt._write_(_dynapyt_ast_, 14, _rt._read_(_dynapyt_ast_, 13, lambda: y), [lambda: z])
-        y = _rt._write_(_dynapyt_ast_, 16, _rt._read_(_dynapyt_ast_, 15, lambda: x), [lambda: y])
-        return _rt._read_(_dynapyt_ast_, 17, lambda: y) 
-        _rt._func_exit_(_dynapyt_ast_, 0, "slice_me")
-    
-    _rt._call_(_dynapyt_ast_, 32, _rt._read_(_dynapyt_ast_, 18, lambda: slice_me), False, [], {})
-except Exception as _dynapyt_exception_:
-    _rt._catch_(_dynapyt_exception_)
+slice_me()
