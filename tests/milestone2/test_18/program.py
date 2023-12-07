e6c3be394dd8269ab031ae23602a05d06382aaa4
Mahdi Jafarkhani <Mahdi.Jafarkhani@Gmail.com>
2023-12-07 11:13:43 +0100

    Add new test

---
+++ tests/milestone2/test_18/program.py
@@ -0,0 +1,17 @@
+class Person:
+    def __init__(self, name, age, weight):
+        self.name = name
+        self.age = age
+        self.weight = weight
+            
+def slice_me():
+    mahdi = Person('Mahdi', 25, 65)
+    possible_ages = [10, 20, 30, 40, 50]
+    mahdi.name = 'Mehdi'
+    mahdi.weight = mahdi.age * 2
+    mahdi.age += possible_ages[2]
+    mahdi.age *= mahdi.weight * possible_ages[3]
+    possible_ages.clear()
+    return mahdi.age # slicing criterion
+
+slice_me()
