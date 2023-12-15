fd1da9c93ac3a9f3745cf3452732c9dff28f3ba5
Mahdi Jafarkhani <Mahdi.Jafarkhani@Gmail.com>
2023-12-15 15:18:36 +0100

    Add new Test Case

---
+++ tests/milestone2/test_25/expected.py
@@ -0,0 +1,7 @@
+def slice_me():
+    x = [1,2,3,4,5]
+    x[0] = 20
+
+    print(x) # slicing criterion
+
+slice_me()
---
+++ tests/milestone2/test_25/program.py
@@ -0,0 +1,12 @@
+def slice_me():
+    x = [1,2,3,4,5]
+    x[0] = 10
+    y = 2
+    z = x[0] + y
+    x[0] = 20
+    
+    print(z)
+    print(x) # slicing criterion
+    print(y)
+
+slice_me()
